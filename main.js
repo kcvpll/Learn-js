aa6e8bc166a8805d422858c94bec84e49f321cfb
kcvpll <korsun.marketing@gmail.com>
2019-01-19 20:14:33 +0300

    работа с числами

--- main.js
+++ main.js
@@ -1,12 +1,20 @@
-var myNumber = 10; // числа
-var myString = "Привет, мир"; //строковое значение 
-var myBoolean = true; // булевое значение, которое принимает знаечение истина/ложь
-var myNull = null; // у переменной присвоено значение, но оно является ничему 
-var myUndefined = undefined; // у переменной значение отсутствует
-var myObject = (
-	name:  "Pavel",
-	age: 19
-);  // некий составной тип, который может содержать свойства, и каждое из свойств может иметь различные типы  
+//Числа 
+var result = 40 + 10; // все действия выполняются справа на лево, сперва складываются числа и присваиваются переменной resut
+var myNumber = 100; 
+
+console.log(result + myNumber); 
+console.log(result - myNumber); 
+console.log(result * myNumber); 
+console.log(result / myNumber); 
+
+myNumber = myNumber + 1; // увеличиваем значение переменной на 1
+//аналогичная запись: myNumber += 1 или же myNumber ++ 
+console.log(myNumber);
+
+console.log(Math.round(5.45)); //переменная Math.round округляет до целого числа
+console.log(Math.ceil(5.45)); // округляет в большую сторону
+console.log(Math.floor(5.45)); // округляет в меньшую сторону
 
 
-alert(myString);
+var newNumber = 2.985; 
+console.log(newNumber.toFixed(1));
