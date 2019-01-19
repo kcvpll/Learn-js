b229dbb1c38842d08d470b951490305378411bc9
kcvpll <korsun.marketing@gmail.com>
2019-01-19 20:23:58 +0300

    работа со строками

--- main.js
+++ main.js
@@ -1,20 +1,11 @@
-//Числа 
-var result = 40 + 10; // все действия выполняются справа на лево, сперва складываются числа и присваиваются переменной resut
-var myNumber = 100; 
+//var newNumber = 2.985; 
+//console.log(newNumber.toFixed(1)); // после точки выводит 1 знак + округление
 
-console.log(result + myNumber); 
-console.log(result - myNumber); 
-console.log(result * myNumber); 
-console.log(result / myNumber); 
+// Работа со строками
 
-myNumber = myNumber + 1; // увеличиваем значение переменной на 1
-//аналогичная запись: myNumber += 1 или же myNumber ++ 
-console.log(myNumber);
-
-console.log(Math.round(5.45)); //переменная Math.round округляет до целого числа
-console.log(Math.ceil(5.45)); // округляет в большую сторону
-console.log(Math.floor(5.45)); // округляет в меньшую сторону
-
-
-var newNumber = 2.985; 
-console.log(newNumber.toFixed(1));
+var myNumber = 40 + 10;
+var myString = "Привет, ";
+console.log( "40" + myNumber); //вывод 4050, т.к. "" - это строка 
+console.log (myString + "как дела?"); 
+console.log (myString.toLowerCase()); // переводим всё в нижний регистр (ПРИВЕТ - привет);
+console.log (myString.toUpperCase()); // переводим всё в верхний регистр (привет - ПРИВЕТ);
