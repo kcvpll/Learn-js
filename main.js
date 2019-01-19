b0a149290c1e63114d1a9042a7e0ecf756e35ea5
kcvpll <korsun.marketing@gmail.com>
2019-01-19 20:33:01 +0300

    работа с массивами

--- main.js
+++ main.js
@@ -5,7 +5,16 @@
 
 var myNumber = 40 + 10;
 var myString = "Привет, ";
-console.log( "40" + myNumber); //вывод 4050, т.к. "" - это строка 
-console.log (myString + "как дела?"); 
-console.log (myString.toLowerCase()); // переводим всё в нижний регистр (ПРИВЕТ - привет);
-console.log (myString.toUpperCase()); // переводим всё в верхний регистр (привет - ПРИВЕТ);
+// console.log( "40" + myNumber); //вывод 4050, т.к. "" - это строка 
+// console.log (myString + "как дела?"); 
+// console.log (myString.toLowerCase()); // переводим всё в нижний регистр (ПРИВЕТ - привет);
+// console.log (myString.toUpperCase()); // переводим всё в верхний регистр (привет - ПРИВЕТ);
+
+// Работа с массивами 
+
+var names = ["Pavel", "Aleks", "Salmon", "Dmitriy"]; 
+//var numbers = [1,2,3,4];
+//console.log(names[3].toUpperCase()); // получаем первый идекс из массива - Алекс
+names[1] = "Masha"; // меняем то, что находится под индексом 1
+names.push("Sveta"); // добавляем в конец массива имя Света
+console.log(names);
